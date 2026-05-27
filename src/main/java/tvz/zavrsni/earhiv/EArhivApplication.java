4dfc8aab98c74660da98023ed31fe5b76f8cbcdb
ivanjur03 <ivanjurjevic03@gmail.com>
2026-05-27 14:40:51 +0200

    Initial commit

---
+++ src/main/java/tvz/zavrsni/earhiv/EArhivApplication.java
@@ -0,0 +1,13 @@
+package tvz.zavrsni.earhiv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EArhivApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EArhivApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tvz/zavrsni/earhiv/EArhivApplicationTests.java
@@ -0,0 +1,13 @@
+package tvz.zavrsni.earhiv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EArhivApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
